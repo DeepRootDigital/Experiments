df9b663344cee8426f73802431bd99e98f00d5b3
Business On Market St. <admin@businessonmarketst.com>
2013-01-31 15:31:49 -0800

    Goldgrabber!

---
+++ goldgrabber/function.php
@@ -0,0 +1,18 @@
+<?php
+
+function gold_scrap_cal($qty, $purepercent) {
+    $goldfeed = file_get_contents('http://gold-feed.com/iframe/gold-feed-plain.php');
+    $metalprice = $goldfeed / 28.3495;
+    $preresult = number_format($metalprice, 1);
+    $result2 = $preresult * $purepercent / 100;
+    $result = number_format($result2, 1) * $qty;
+    return $result;
+}
+?>
+
+
+<div class="goldgrabber">
+    <form action="process.php" method="get" />
+    
+</form>
+</div>
