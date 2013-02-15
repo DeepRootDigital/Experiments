8c769300a6153a2a6505895f60d35514b237a237
Ray Sinlao <rsinlao86@gmail.com>
2013-02-14 17:30:52 -0800

    Updates Winners Circle

---
+++ winners-circle/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Winners Circle</title>
+	<link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/2eb637b8-6ca1-4d92-b8b6-234adfb90635.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
+</head>
+<body>
+	<div class="page-wrap">
+		<div class="center canvas">
+			<header>
+				<nav>
+					<ul>
+						<li><a href="#">Apartment Homes</a></li>
+						<li><a href="#">Amenities</a></li>
+						<li><a href="#">Community</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="#">Faq</a></li>
+					</ul>
+				</nav>
+				<div class="clear"></div>
+				<div class="social-media">
+					<ul>
+						<li><a href="#">F</a></li>
+						<li><a href="#">L</a></li>
+						<li><a href="#">X</a></li>
+					</ul>
+					<div class="clear"></div>
+				</div>
+				<div class="tour-contact"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png"></div>
+				<div class="tour-contact-bottom"><img src="<?php echo get_template_directory_uri(); ?>/images/logo-bottom.png"></div>
+			</header>
+			<div class="slide"><img src="<?php echo get_template_directory_uri(); ?>/images/slide1.jpg"></div>
+			<div class="home-options">
+				<div class="home-option-img">
+					<ul>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/images/bedroom1.png"></li>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/images/bedroom2.png"></li>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/images/bedroom3.png"></li>
+						<li><img src="<?php echo get_template_directory_uri(); ?>/images/bedroom4.png"></li>
+					</ul>
+					<div class="clear"></div>
+				</div>
+				
+				<div class ="home-option-select">
+					<ul>
+						<li><a href="#">One<br><span>Bedroom</span></a></li>
+						<li><a href="#">Two<br><span>Bedroom</span></a></li>
+						<li><a href="#">Three<br><span>Bedroom</span></a></li>
+						<li><a href="#">1 <span>&amp;</span> 2 floor<br><span>options</span></a></li>
+					</ul>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="home-content">
+				<div class="home-col-1 home-col">
+					<h2><em>Apply</em> Now</h2>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/content-underline.png">
+					<p>Lorem ipsum dolor sit amet, essent postulant ea ius, ut idque invidunt quo. Dicunt neglegentur ei usu, magna numquam at mel.<br><br> Pico voluptatum eos at. Ne sed primis deseruisse, tation habemus corrumpit in nam, eu legere possit principes.</p>
+					<a href="#">Learn <em>more</em><img src="<?php echo get_template_directory_uri(); ?>/images/learn-more-arrow.png"></a>
+				</div>
+				<div class="home-col-2 home-col">
+					<h2>Our <em>Amenities</em></h2>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/content-underline.png">
+					<p>Lorem ipsum dolor sit amet, essent postulant ea ius, ut idque invidunt quo. Dicunt neglegentur ei usu, magna numquam at mel.<br><br> Pico voluptatum eos at. Ne sed primis deseruisse, tation habemus corrumpit in nam, eu legere possit principes.</p>
+					<a href="#">Learn <em>more</em><img src="<?php echo get_template_directory_uri(); ?>/images/learn-more-arrow.png"></a>
+				</div>
+				<div class="home-col-3 home-col">
+					<h2><em>Schedule</em> a Tour</h2>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/content-underline.png">
+					<p>Lorem ipsum dolor sit amet, essent postulant ea ius, ut idque invidunt quo. Dicunt neglegentur ei usu, magna numquam at mel.<br><br> Pico voluptatum eos at. Ne sed primis deseruisse, tation habemus corrumpit in nam, eu legere possit principes.</p>
+					<a href="#">Learn <em>more</em><img src="<?php echo get_template_directory_uri(); ?>/images/learn-more-arrow.png"></a>
+				</div>
+			</div>
+		</div><!-- END Canvas -->
+		<footer>
+			<div class="center">
+				<div class="widgets-container">
+					<div class="footer-widget">
+						<h2>Looking to <em>buy?</em></h2>
+						<img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png">
+					</div>
+					<div class="footer-widget blog-widget">
+						<h2>WC <em>Blog</em></h2>
+						<ul>
+							<li><a href="#">Interesting Blog Entry</a></li>
+							<li class="dotted-line">.....................................................................</li>
+							<li><a href="#">Cool, Another Article</a></li>
+							<li class="dotted-line">.....................................................................</li>
+							<li><a href="#">This is a cool post</a></li>
+							<li class="dotted-line">.....................................................................</li>
+							<li><a href="#">A good article is worth</a></li>
+							<li class="dotted-line">.....................................................................</li>
+							<li><a href="#">Blog Content Helps SEO</a></li>
+						</ul>
+					</div>
+					<div class="footer-widget social-media-widget">
+						<h2>Social <em>Media</em></h2>
+						<ul>
+							<li><a href="#">L</a></li>
+							<li><a href="#">F</a></li>
+							<li><a href="#">&</a></li>
+							<li><a href="#">X</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</footer>
+		<div class="footer-lower">
+			<div class="center"> 
+				<p>Dico voluptatum eos at. Ne sed primis deseruisse, tation habemus corrumpit in nam, eu legere possit principes.</p>
+				<a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/footer-lower-logo.png"></a>
+			</div>
+		</div>
+	</div><!-- END Page Wrap -->
+	<script type="text/javascript" src="http://fast.fonts.com/jsapi/2eb637b8-6ca1-4d92-b8b6-234adfb90635.js"></script>
+</body>
+</html>
