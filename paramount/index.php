22f1c2cbe4fe16220c8cfb0ec5a6b4fa40817bf1
Dan Deller <deller26@gmail.com>
2013-02-14 17:26:29 -0800

    finished paramount

---
+++ paramount/index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Paramount</title>
+        <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet" type="text/css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link type="text/css" rel="stylesheet" href="http://fast.fonts.com/cssapi/7fae6fe2-5b59-4099-96aa-619fda1b22d5.css"/>
+        <script type="text/javascript" src="http://fast.fonts.com/jsapi/7fae6fe2-5b59-4099-96aa-619fda1b22d5.js"></script>
+
+        <script src="<?php echo get_template_directory_uri(); ?>/paramount.js" type="text/javascript"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" width="347" height="85" alt="logo"/>
+                <img class="phoneinfo" src="<?php echo get_template_directory_uri(); ?>/images/phoneinfo.png" width="164" height="22" alt="phoneinfo"/>
+                <p>SIGN UP FOR OUR NEWSLETTER</p>
+                <input type="text" name="email" value="email" />
+                <img class="gobutton" src="<?php echo get_template_directory_uri(); ?>/images/gobutton.png" width="33" height="29" alt="gobutton"/>
+            </div>
+
+            <div class="content">
+                <img class="homeimage" src="<?php echo get_template_directory_uri(); ?>/images/homeimage.png" width="18" height="17" alt="homeimage"/>
+                <div class="nav">
+                    <ul>
+                        <li>ABOUT US</li>
+                        <li>SERVICES</li>
+                        <li>BLOG</li>
+                        <li>SUCCESS STORIES</li>
+                        <li>FREE EVALUATION</li>
+                        <li>CONTACT US</li>
+                    </ul>
+
+                    <div class="topsocialmedia">                  
+                        <ul><!-- DON'T FORGET THE ANIMATED CLASSES BELLOW! -->
+                            <li><img class="link" src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png" width="35" height="37" alt="facebook"/></li>
+                            <li><img src="<?php echo get_template_directory_uri(); ?>/images/pintrest2.png" alt="facebook"/></li>
+                            <li><img class="twit" src="<?php echo get_template_directory_uri(); ?>/images/twitter.png" width="35" height="37" alt="facebook"/></li>
+                            <li><img class="twit" src="<?php echo get_template_directory_uri(); ?>/images/facebook.png" width="35" height="37" alt="facebook"/></li>
+                        </ul>                   
+                    </div>
+                </div>
+
+                <div class="slider"></div>
+                <div class="submitbox">
+                    <div class="submitboxtext">
+                        PROPERTY<br />
+                        TAX<br />
+                        REDUCTION
+                        <br />
+                        <br />
+                        <br />
+                        <p><i>FEASIBILITY<br /> STUDY</i></p>
+                        <input type="text" name="name" value="name" />
+                        <input type="text" name="email" value="email" />
+                        <input type="text" name="phone" value="phone" />
+                        <img class="submitbutton" src="<?php echo get_template_directory_uri(); ?>/images/submitbutton.png" width="199" height="36" alt="submitbutton"/>
+                        <span style="font-family: Helvetica Neue ; float: left ; font-size: 10px ; color: #bcbcbc ; margin-top: 10px; ; margin-left: -6px">[ No Cost ]</span>                      
+                    </div>
+                </div>
+
+                <div class="bodycontent1">
+                    <div class="connect">
+                        <div class="bluebox"></div>
+                        <p>Connect</p>
+                    </div>
+
+                    <div class="appeal">
+                        <div class="bluebox"></div>
+                        <p>Appeal</p>
+                    </div>
+
+                    <div class="refund">
+                        <div class="bluebox"></div>
+                        <p>Refund</p>
+                    </div>
+
+                    <div class="represent">
+                        <div class="bluebox"></div>
+                        <p>Represent</p>
+                    </div>
+
+                    <div class="evaluate">
+                        <div class="bluebox"></div>
+                        <p>Evaluate</p>
+                    </div>
+
+                    <div class="engage">
+                        <div class="bluebox"></div>
+                        <p>Engage</p>
+                    </div>
+                </div>
+
+                <div class="bodycontent2">
+                    <div class="lefttext">
+                        <h2>HOW ARE WE DIFFERENT?</h2>
+                        <hr />
+                        <p>We pride our selves on our team of experts who are here to service your needs 
+                            and provide the results you deserve. Property tax appeal is just the first 
+                            step, our goal is to build a long term relationship with you by servicing all 
+                            of your real estate needs. We work on a Performance Based Fee, which means 
+                            that if we don’t save you anything, you pay nothing. Or if you prefer, we 
+                            will work on an hourly fee basis or flat rate fee.</p>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <h2>WHO IS PARAMOUNT?</h2>
+                        <hr />
+                        <p>Paramount is more than a commercial brokerage firm. Our Property Tax Appeal 
+                            Division specializes in reducing secured and unsecured property tax liabilities. 
+                            Our staff, which includes former supervising county appraisers and senior brokers,
+                            has helped reduce the tax burden in over 10,000 transactions. When property taxes
+                            account for 20% – 30% of building owners operating expenses the reduced burden is
+                            significant. The Tax Appeal process is very complex and time consuming, but with
+                            Paramount on your side you’ll save time, money and effort by receiving professional
+                            representation and an appraisal that uses multiple approaches to property valuation
+                            for the Assessment Appeals Board.</p>
+                    </div>
+
+                    <div class="rightlogo">
+                        <img src="<?php echo get_template_directory_uri(); ?>/images/rightlogo.png" width="192" height="225" alt="rightlogo"/>
+                    </div>
+                </div>
+
+                <div class="bodycontent3">
+                    <div class="quote">
+                        <p>
+                            "<span style="color: #154174">PARAMOUNT</span> SIGNIFICANTLY REDUCED MY TAX BILL. THEIR CONSTANT COMMUNICATION 
+                            AND EXPERTISE MADE THE TAX APPEAL PROCESS PAINLESS FOR ME. 
+                            <br />
+                            <span style="color: #9d9d9d ; font-size: 24px">– GEORGE MAYER, COASTLINE EQUITY</span>
+                        </p>
+                    </div>
+
+                    <div class="sumbitbox2">
+                        <div class="submitbox2text">
+                            PROPERTY<br />
+                            TAX<br />
+                            REDUCTION
+                            <br />
+                            <br />
+                            <br />
+                            <p><span style="font-size: 15px ; margin-top: -20px;"><i>FEASIBILITY<br /> STUDY</i></span></p>
+                            <input type="text" name="name" value="name" />
+                            <input type="text" name="email" value="email" />
+                            <input type="text" name="phone" value="phone" />
+                            <img class="submitbutton2" src="<?php echo get_template_directory_uri(); ?>/images/submitbutton.png" width="199" height="36" alt="submitbutton"/>
+                            <span style="font-family: Helvetica Neue ;float: left ; font-size: 10px ; color: #bcbcbc ; margin-top: 10px; ; margin-left: -6px">[ No Cost ]</span> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer">
+                <div class="bottomnav">
+                    <ul>
+                        <li>HOME</li>
+                        <li>ABOUT US</li>
+                        <li>SERVICES</li>
+                    </ul>
+                    <ul>
+                        <li>BLOG</li>
+                        <li>FREE EVALUATION</li>
+                        <li>CONTACT US</li>
+                    </ul>
+                </div> 
+
+                <div class="blogfeed">
+                    <span style="color: #154174 ; font-size: 18px ; margin-left: 14px;">LATEST BLOG FEED</span>
+                    <input type="text" name="place holder" value="place holder " />
+                </div>
+
+                <div class="rightfootercontent">
+                    <ul><!-- DON'T FORGET THE ANIMATED CLASSES BELLOW! -->
+                        <li><img class="face" src="<?php echo get_template_directory_uri(); ?>/images/facebook.png" width="35" height="37" alt="facebook"/></li>
+                        <li><img class="twit" src="<?php echo get_template_directory_uri(); ?>/images/twitter.png" width="35" height="37" alt="facebook"/></li>
+                        <li><img src="<?php echo get_template_directory_uri(); ?>/images/pintrest.png" alt="pintrest"/></li>
+                        <li><img class="link" src="<?php echo get_template_directory_uri(); ?>/images/linkedin.png" width="35" height="37" alt="facebook"/></li>
+                    </ul>
+                    <p>SIGN UP FOR OUR NEWSLETTER</p>
+                    <input type="text" name="email" value="email" />
+                    <img class="bottomgobutton" src="<?php echo get_template_directory_uri(); ?>/images/gobutton.png" width="33" height="29" alt="gobutton"/>
+                    <img class="bottomphoneinfo" src="<?php echo get_template_directory_uri(); ?>/images/phoneinfo.png" width="164" height="22" alt="phoneinfo"/>
+                </div>
+                <div class="bottomtexture"></div>
+            </div>
+    </body>
+</html>
