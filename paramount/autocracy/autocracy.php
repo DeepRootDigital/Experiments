456424ef2ec71fdb1bb741a77190d10be8c6f6f6
Nicholas Koskowski <Nick@businessonmarketst.com>
2013-03-01 14:46:55 -0800

    added backend API's for homepage

---
+++ paramount/autocracy/autocracy.php
@@ -0,0 +1,18 @@
+<?php
+add_action('admin_init', 'theme_options_init');
+add_action('admin_menu', 'theme_options_add_page');
+/**
+ * Init plugin options to white list our options
+ */
+function theme_options_init() {
+    register_setting('sample_options', 'sample_theme_options', 'theme_options_validate');
+    register_setting('main_options', 'main_theme_options', 'main_theme_options_validate');
+    }
+/**
+ * Load up the menu page
+ */
+function theme_options_add_page() {
+    add_theme_page(__('Homepage Manager', 'sampletheme'), __('Homepage Manager', 'sampletheme'), 'edit_theme_options', 'sample_theme_options', 'theme_options_do_page');
+    add_theme_page(__('Theme Manager', 'sampletheme'), __('Theme Manager', 'sampletheme'), 'edit_theme_options', 'main_theme_options', 'main_theme_options_do_page');
+}
+?>
--- paramount/functions.php
+++ paramount/functions.php
@@ -1,9 +1,17 @@
 <?php
+require_once ( get_template_directory() . '/autocracy/autocracy.php' );
+require_once ( get_template_directory() . '/autocracy/homepage-manager.php' );
+require_once ( get_template_directory() . '/autocracy/theme-manager.php' );
+// Re-define meta box path and URL
+define('RWMB_URL', trailingslashit(get_stylesheet_directory_uri() . '/autocracy/API/'));
+define('RWMB_DIR', trailingslashit(get_template_directory() . '/autocracy/API/'));
+// Include the meta box script
+require_once RWMB_DIR . '/meta-box.php';
+include get_template_directory() . '/autocracy/demo.php';
 add_theme_support('menus');
 register_nav_menu('Header Nav', 'Navigation Menu');
 register_nav_menu('Footer Nav Left', 'Footer Menu Left');
 register_nav_menu('Footer Nav Right', 'Footer Menu Right');
-
 function create_post_type() {
 	register_post_type('Services', array(
 		'labels' => array(
