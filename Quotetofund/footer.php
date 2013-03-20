5676f0228d1501d390a1e614179819954c115eab
Dan Deller <deller26@gmail.com>
2013-03-20 15:55:03 -0700

    Quote 2 fund
    Dan's styles/Header/Footer

---
+++ Quotetofund/footer.php
@@ -0,0 +1,13 @@
+<div class="container">
+<div class="footer-content">
+	<img class="footer-logo" src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" alt="logo"/>
+	<nav class="bottom-nav">
+     		<ul>
+     			<li><a href = "#">quote</a></li>
+     			<li><a href = "#">about us</a></li>
+     			<li><a href = "#">blog</a></li>
+     			<li><a href = "#">contact</a></li>
+     		</ul>
+     	</nav>
+</div>
+</div>
---
+++ Quotetofund/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+         <link href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" rel="stylesheet" /> 
+     </head>
+
+     <body>
+     	<div class="container">
+     <div class="header">
+     	<img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="logo"/>
+     	<nav class="top-nav">
+     		<ul>
+     			<li><a href = "#">quote</a></li>
+     			<li><a href = "#">about us</a></li>
+     			<li><a href = "#">blog</a></li>
+     			<li><a href = "#">contact</a></li>
+     		</ul>
+     	</nav>
+     </div>
+
+ </div>
