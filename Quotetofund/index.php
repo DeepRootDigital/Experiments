8e1ecb27c88c739d5542a6bd30f5b4226e319f0a
Nicholas Koskowski <Nick@businessonmarketst.com>
2013-03-20 16:25:07 -0700

    added more to  q2f
    q2f

--- Quotetofund/index.php
+++ Quotetofund/index.php
@@ -1,2 +1,39 @@
-<?php get_header(); ?>
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quote To Fund</title>
+	<link href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<div class="container">
+		<div id="slider">
+		</div>
+		<div id="main-content">
+			<div class="content-box-wrap">
+				<div class="content-box">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/one.png" alt="step-one">
+					<h3>Kindly Share your Monthly Bank Deposit</h3>
+					<input type="text">
+					<p>Vel an velit feugait, meis ignota vis ei. Cu oblique maiorum mel, no quaeque interpretaris quo.</p>
+				</div>
+				<div class="content-box">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/two.png" alt="step-two">
+					<h3>Your monthly credit card sales are?</h3>
+					<input type="text">
+					<p>Vel an velit feugait, meis ignota vis ei. Cu oblique maiorum mel, no quaeque interpretaris quo.</p>
+				</div>
+				<div class="content-box">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/three.png" alt="step-three">
+					<h3>Finally We just need your email?</h3>
+					<input type="text">
+					<p>Vel an velit feugait, meis ignota vis ei. Cu oblique maiorum mel, no quaeque interpretaris quo.</p>
+				</div>
+			</div>
+		</div>
+		<div id="bottom-content">
+			<div id="bottom-sidebar">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
