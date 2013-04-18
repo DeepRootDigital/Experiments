cc8ce610d4076a773c0e9c6c4d722fc42ed961f7
Dan Deller <deller26@gmail.com>
2013-04-17 18:22:58 -0700

    Quote 2 Fund
    Updates

--- Quotetofund/blog-summary.php
+++ Quotetofund/blog-summary.php
@@ -7,18 +7,18 @@ Template Name: blog-summary
 
 <div class="blog-summary-background">
 	<section class="blog-summary-content">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/blog-business-finance.png" />
+		<img class="blog-ribbon" src="<?php echo get_template_directory_uri(); ?>/images/blog-business-finance.png" />
 		<h1>blog article <span>title</span></h1>
 		<div class="blog-posted-by">
 			<p>posted by Justin Smith</p>
 		</div>
 		<img class="blog-summary-dotted-bar" src="<?php echo get_template_directory_uri(); ?>/images/bottom-body-dotted-line.png" />
 		<div class="blog-summary-main-image blog-top-image">
-		<img class="blog-posted-date" src="<?php echo get_template_directory_uri(); ?>/images/blog-posted-date.png" />
+			<img class="blog-posted-date" src="<?php echo get_template_directory_uri(); ?>/images/blog-posted-date.png" />
 		</div>
 		<div class="blog-description">
-		<p> Est legere argumentum dissentiet ea, per elit nihil et. Quo quodsi accusata suavitate in, dicit audiam inciderint ei vix. An consulatu prodesset...</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/images/blog-more-button.png" />
+			<p> Est legere argumentum dissentiet ea, per elit nihil et. Quo quodsi accusata suavitate in, dicit audiam inciderint ei vix. An consulatu prodesset...</p>
+			<img src="<?php echo get_template_directory_uri(); ?>/images/blog-more-button.png" />
 		</div>
 		<ul>
 			<li>tags</li>
@@ -30,18 +30,18 @@ Template Name: blog-summary
 		<img class="blog-bottom-border" src="<?php echo get_template_directory_uri(); ?>/images/blog-bottom-border.png" />
 	</section>
 	<section class="blog-summary-content">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/blog-business-finance.png" />
+		<img class="blog-ribbon" src="<?php echo get_template_directory_uri(); ?>/images/blog-business-finance.png" />
 		<h1>blog article <span>title</span></h1>
 		<div class="blog-posted-by">
 			<p>posted by Justin Smith</p>
 		</div>
 		<img class="blog-summary-dotted-bar" src="<?php echo get_template_directory_uri(); ?>/images/bottom-body-dotted-line.png" />
 		<div class="blog-summary-main-image blog-top-image">
-		<img class="blog-posted-date" src="<?php echo get_template_directory_uri(); ?>/images/blog-posted-date.png" />
+			<img class="blog-posted-date" src="<?php echo get_template_directory_uri(); ?>/images/blog-posted-date.png" />
 		</div>
 		<div class="blog-description">
-		<p> Est legere argumentum dissentiet ea, per elit nihil et. Quo quodsi accusata suavitate in, dicit audiam inciderint ei vix. An consulatu prodesset...</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/images/blog-more-button.png" />
+			<p> Est legere argumentum dissentiet ea, per elit nihil et. Quo quodsi accusata suavitate in, dicit audiam inciderint ei vix. An consulatu prodesset...</p>
+			<img src="<?php echo get_template_directory_uri(); ?>/images/blog-more-button.png" />
 		</div>
 		<ul>
 			<li>tags</li>
@@ -53,18 +53,18 @@ Template Name: blog-summary
 		<img class="blog-bottom-border" src="<?php echo get_template_directory_uri(); ?>/images/blog-bottom-border.png" />
 	</section>
 	<section class="blog-summary-content">
-		<img src="<?php echo get_template_directory_uri(); ?>/images/blog-business-finance.png" />
+		<img class="blog-ribbon" src="<?php echo get_template_directory_uri(); ?>/images/blog-business-finance.png" />
 		<h1>blog article <span>title</span></h1>
 		<div class="blog-posted-by">
 			<p>posted by Justin Smith</p>
 		</div>
 		<img class="blog-summary-dotted-bar" src="<?php echo get_template_directory_uri(); ?>/images/bottom-body-dotted-line.png" />
 		<div class="blog-summary-main-image blog-top-image">
-		<img class="blog-posted-date" src="<?php echo get_template_directory_uri(); ?>/images/blog-posted-date.png" />
+			<img class="blog-posted-date" src="<?php echo get_template_directory_uri(); ?>/images/blog-posted-date.png" />
 		</div>
 		<div class="blog-description">
-		<p> Est legere argumentum dissentiet ea, per elit nihil et. Quo quodsi accusata suavitate in, dicit audiam inciderint ei vix. An consulatu prodesset...</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/images/blog-more-button.png" />
+			<p> Est legere argumentum dissentiet ea, per elit nihil et. Quo quodsi accusata suavitate in, dicit audiam inciderint ei vix. An consulatu prodesset...</p>
+			<img src="<?php echo get_template_directory_uri(); ?>/images/blog-more-button.png" />
 		</div>
 		<ul>
 			<li>tags</li>
@@ -76,12 +76,12 @@ Template Name: blog-summary
 		<img class="blog-bottom-border blog-last-border" src="<?php echo get_template_directory_uri(); ?>/images/blog-bottom-border.png" />
 	</section>
 
-<div class="blog-summary-side-menu">
-<?php get_template_part('blogfeed','sidemenu'); ?>
-</div>
-<div class="blog-summary-feed">
-<?php get_template_part('sidebar','blogfeed'); ?>
-</div>
+	<div class="blog-summary-side-menu">
+		<?php get_template_part('blogfeed','sidemenu'); ?>
+	</div>
+	<div class="blog-summary-feed">
+		<?php get_template_part('sidebar','blogfeed'); ?>
+	</div>
 </div>
 
 
