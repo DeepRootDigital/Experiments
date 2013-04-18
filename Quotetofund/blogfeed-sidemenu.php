9e245a7470416b3df0799052117f802a6d6050ae
Dan Deller <deller26@gmail.com>
2013-04-17 18:03:46 -0700

    Quote 2 Fund
    Blog page summary.

---
+++ Quotetofund/blogfeed-sidemenu.php
@@ -0,0 +1,11 @@
+<aside class="sub-page-right-menu">
+	<div class="side-menu top">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/atf-arrow.png" />
+		<p>calculate <span>approval</span> <em>estimates</em></p>
+	</div>
+	<img class="sub-page-menu-divider" src="<?php echo get_template_directory_uri(); ?>/images/sub-page-menu-divider.png" />
+	<div class="side-menu bottom">
+		<p>calculate <span>payments</span></p>
+		<img class="side-bottom" src="<?php echo get_template_directory_uri(); ?>/images/qtf-white-arrow.png" />
+	</div>
+</aside>
