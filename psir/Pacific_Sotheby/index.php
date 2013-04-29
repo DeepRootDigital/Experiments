f40dd60086f838a923742a502f04ea179226dd2c
Dan Deller <deller26@gmail.com>
2013-04-29 16:32:10 -0700

    PSIR
    About us information

---
+++ psir/Pacific_Sotheby/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pacific Sotherby's</title>
+	<style type="text/css">
+	p,h1,h2,h3,h4,h5,span,em {color: #000}
+	body {border: 0px; padding: 0px; margin: 0px}
+	.container {margin: 0 auto 0 auto; width: 960px; height: 1700px; color: #fff}
+	.pacific-city-image {display: block; margin-left: auto; margin-right: auto;}
+	.pacific-mini-nav {float: left; width: 860px; height: 73px; background-image: url('images/pacific-mini-nav-background.png'); margin: -5px 0 0 50px;}
+	.pacific-mini-nav ul {position: relative; top: 6px; list-style-type: none; margin: 0 0 0 0; font-family: gill sans; text-transform: uppercase;}
+	.pacific-mini-nav ul li { margin: 0 0 0 -40px}
+	.pacific-mini-nav ul li a{ float: left; text-decoration: none; color: #949292; height: 42px ; width: 215px; text-align: center; padding: 25px 0 0 0}
+	.pacific-mini-nav ul li a:hover {color: #fff; background-color: #3a3837; width: 213px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; border-left: 1px solid #646161; border-right: 1px solid #646161}
+	.pacific-about-us {float: left; width: 860px; height:315px; margin: 0 0 0 50px; border-bottom: 4px solid black }
+	.pacific-about-us h3 {text-align: center; font-family: gill sans; font-size: 15px; margin: 15px 0 0 0; color: #464546}
+	.about-us-top-lines {float: left; margin: -8px 0 0 0}
+	.pacific-about-us p {font-family: arial; font-size: 13px; color: #464546}
+	.profile-section {float: left; width: 860px; height: 220px; margin: 15px 0 0 50px; background-color: #f2f2f2}
+	.person-info {position: relative; float: left; width: 430px; height: 220px}
+	.person-info img {position: absolute; top: 9px;}
+	.person-description { position: absolute; width: auto; height: auto; top: -5px; left: 180px}
+	.person-description h2 {font-family: gill sans; font-weight: normal; color: #0082c9}
+	.person-description p {position: relative; top: -20px; font-family: arial; font-size: 15px}
+	.person-description p:last-child {top: -32px; font-size: 13px; font-style: italic}
+	#ca-dre-info {margin-top: -10px}
+	#person-location {margin-top: -10px}
+	</style>
+</head>
+<body>
+	<section class="container">
+		<img class="pacific-city-image" src="images/pacific-city-image.png" />
+		<div class="pacific-mini-nav">
+			<ul>
+				<li><a href="#">leadership</a></li>
+				<li><a href="#">office management</a></li>
+				<li><a href="#">marketing</a></li>
+				<li><a href="#">support team</a></li>
+			</ul>
+		</div>
+		<article class="pacific-about-us">
+			<h3>About Pacific Sotheby's International Realty</h3>
+			<img class="about-us-top-lines" src="images/about-us-top-lines.png" />
+			<p>Pacific Sotheby’s International Realty provides our clients with comprehensive real estate services offering years of expertise, in–depth market knowledge and the resources of a global network.</p>
+			<p>Through our partnership with Sotheby’s International Realty, we are able to place your home on a level above any other firm in San Diego County by offering extraordinary marketing partnerships with some of the strongest media outlets in the world. </p>
+			<p>Specializing in the sale of luxury properties throughout San Diego, Pacific Sotheby’s International Realty understands that to market a distinct home requires uncommon knowledge and resources. Our agents are recognized as industry leaders and experts in the San Diego and Southern California marketplace and are uniquely positioned to assist our clients to successfully meet their real estate objectives. </p>
+			<p>Personal Relationships define Pacific Sotheby’s International Realty. Our Southern California real estate professionals understand that building a lasting relationship through excellent service and caring is the most important component of our success.</p>
+			<p>Pacific Sotheby’s Realty agents truly understand that a home, regardless of its price, holds and reflects the value of your life’s investment. We take great pride in using our expertise, resources and global connections to perfectly unite extraordinary places with the extraordinary buyers who will cherish them as we do.</p>
+		</article>
+	
+		<section class="profile-section">
+			<div class="person-info">
+				<img src="images/communication-director.png" />
+				<div class="person-description">
+					<h2>Colby Arrington</h2>
+					<p>Communication Director</p>
+					<p>619.871.1321</p>
+					<p>CArrington@PacificSIR.com</p>
+				</div>
+			</div>
+			<div class="person-info">
+				<img src="images/chairman.png" />
+				<div class="person-description">
+					<h2>Steve Games</h2>
+					<p>Chairman of the Board</p>
+					<p>858.926.3060</p>
+					<p>SCGames@PacificSIR.com</p>
+				</div>
+			</div>
+		</section>
+		<section class="profile-section">
+			<div class="person-info">
+				<img src="images/founder-ceo.png" />
+				<div class="person-description">
+					<h2>Brian Arrington</h2>
+					<p>Founding Partner & CEO</p>
+					<p id="ca-dre-info">CA DRE# 01126410</p>
+					<p>619.993.4641</p>
+					<p>brian.arrington@sothbysrealty.com</p>
+				</div>
+			</div>
+			<div class="person-info">
+				<img src="images/assit-branch-manager.png" />
+				<div class="person-description">
+					<h2>Mariana Adan</h2>
+					<p>Assistant Branch Manager</p>
+					<p id="person-location">Point Loma</p>
+					<p id="ca-dre-info">CA DRE#00814799</p>
+					<p>858.926.3060</p>
+					<p>Ineedanemail@PacificSIR.com</p>
+				</div>
+			</div>
+		</section>
+	</section>
+</body>
+</html>
