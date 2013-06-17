b3feed8de0fc14d22c109eff564b7701e9bdc00f
Dan Deller <deller26@gmail.com>
2013-06-17 15:29:17 -0700

    Landing pages...
    landing pages everywhere

---
+++ redline/index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+	<title>Redline Restorations</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div class="container">
+		<div class="logo">
+			<img src="images/logo.png" alt="logo" />
+		</div>
+		<div class="description">
+			<div class="intro-text">
+				<p>A world class automotive restoration facility. 
+					Our TUV-certified staff prides themselves on our extensive research and attention to detail.</p>
+			</div>
+			<div class="enter-email">
+				<p>Enter your email address below now, and we will notify you when we launch our brand new website.</p>
+				<form>
+				<input class="email-text-box" type="text" name="" placeholder="your email" />
+				<input class="submit" type="submit" name="" value="" />
+				</form>
+			</div>
+		</div>
+		<div class="twitter-feed">
+			<div class="feed-section">
+			<p>FOR SALE 1964 PONTIAC GTO CONVERTIBLE</p>
+			<p><a href="#">http://FerrariChat.com http://j.mp/tV1MGm</a></p>
+			</div>
+		</div>
+		<div class="company-info">
+			<img src="images/company-info-line.png" alt="company-info-line" />
+			<div class="block sales">
+				<p>sales @</p>
+				<p><span>redlinerestorations.net</span></p>
+			</div>
+			<div class="block hours">
+				<p><span>m - s</span> 9am - 5pm</p>
+				<p><span>sun</span> by appt</p>
+			</div>
+			<div class="block phone">
+				<p><span>p</span> 203 <em>335 9555</em></p>
+				<p><span>f</span> 203 <em>335 9550</em></p>
+			</div>
+			<div class="block address">
+				<p><em>2316 fairfield ave</em></p>
+				<p>black rock, ct 06605</p>
+			</div>
+		</div>
+		<div class="map">
+			<img src="images/map.png" alt="map" />
+		</div>
+		<div class="social-media">
+			<ul>
+				<li><a href="#"><img src="images/facebook.png" alt="facebook" /></a></li>
+				<li><a href="#"><img src="images/twitter.png" alt="twitter" /></a></li>
+				<li><a href="#"><img src="images/four-square.png" alt="four-square" /></a></li>
+				<li><a href="#"><img src="images/youtube.png" alt="youtube" /></a></li>
+			</ul>
+			<p>Copyright © 2013 Redline Restoration LLC.</p>
+		</div>
+</div>
+</body>
+</html>
