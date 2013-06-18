e631fba4cca1825d89be4722f5a1e9aef8da0fcf
Nicholas Koskowski <Nick@businessonmarketst.com>
2013-06-18 10:06:48 -0700

    Hela Updated Vivioptal Landing Page
    Added wordpress support for deployment and changed some of the project
    schema structure.... GOOD QUALITY CODE DAN!++

--- vivio/landing-page.php
+++ vivio/landing-page.php
@@ -1,26 +1,31 @@
-<!DOCTYPEhtml>
+<?php
+/*
+Template Name: E-Book Landing page
+*/
+?>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Vivioptal</title>
-	<link href="style.css" rel="stylesheet" type="text/css" />
+	<link href="<?php echo get_template_directory_uri(); ?>/lp.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 	<div class="back-ribbon">
 	</div>
 	<div class="container">
 		<div class="top-section">
-			<img class="vivioptal-intro-text" src="images/vivioptal.png" alt="vivioptal" />
+			<img class="vivioptal-intro-text" src="<?php echo get_template_directory_uri(); ?>/lp-images/vivioptal.png" alt="vivioptal" />
 			<div class="e-book-info">
 				<p>download a copy of our</p>
 				<p>free <span>e-book</span></p>
 				<p>about the</p>
 				<p>vitamin b</p>
 				<p>complex</p>
-				<img class="dotted-divider-full" src="images/dotted-divider-full.png" alt="dotted-divider-full" />
-				<img class="dotted-divider-half" src="images/dotted-divider-half.png" alt="dotted-divider-half" />
-				<img class="solid-divider" src="images/solid-divider.png" alt="solid-divider" />
+				<img class="dotted-divider-full" src="<?php echo get_template_directory_uri(); ?>/lp-images/dotted-divider-full.png" alt="dotted-divider-full" />
+				<img class="dotted-divider-half" src="<?php echo get_template_directory_uri(); ?>/lp-images/dotted-divider-half.png" alt="dotted-divider-half" />
+				<img class="solid-divider" src="<?php echo get_template_directory_uri(); ?>/lp-images/solid-divider.png" alt="solid-divider" />
 			</div>
-			<img class="vivio-box-image" src="images/vivio-box-image.png" alt="vivio-box-image" />
+			<img class="vivio-box-image" src="<?php echo get_template_directory_uri(); ?>/lp-images/vivio-box-image.png" alt="vivio-box-image" />
 			<div class="vivio-submit-section">
 				<form>
 					<input type="text" value="first name..." />
@@ -33,7 +38,7 @@
 		<div class="bottom-section">
 			<div class="bottom-section-intro-text">
 				<p>in our <span>e-book</span> you can <span>learn about...</span></p>
-				<img class="bottom-section-intro-divider" src="images/bottom-section-intro-divider.png" alt="bottom-section-intro-divider" />
+				<img class="bottom-section-intro-divider" src="<?php echo get_template_directory_uri(); ?>/lp-images/bottom-section-intro-divider.png" alt="bottom-section-intro-divider" />
 			</div>
 			<div class="b-description font-placement">
 				<h1><span>sources</span> of b vitamins</h1>
@@ -67,12 +72,12 @@
 				<p>800 456 7890</p>
 				<p>info@viviopalus.com</p>
 				<ul>
-					<li><a href="#"><img src="images/facebook-icon.png" alt="" /></a></li>
-					<li><a href="#"><img src="images/twitter-icon.png" alt="" /></a></li>
-					<li><a href="#"><img src="images/youtube-icon.png" alt="" /></a></li>
+					<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/lp-images/facebook-icon.png" alt="" /></a></li>
+					<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/lp-images/twitter-icon.png" alt="" /></a></li>
+					<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/lp-images/youtube-icon.png" alt="" /></a></li>
 				</ul>
 			</div>
-			<img class="footer-divider" src="images/footer-divider.png" alt="footer-divider" />
+			<img class="footer-divider" src="<?php echo get_template_directory_uri(); ?>/lp-images/footer-divider.png" alt="footer-divider" />
 			<p>Copyright © 2012 Vivioptal All rights reserved | Privacy Policy About Our Ads Safety Terms of Service Copyright/IP Policy</p>
 		</div>
 	</div>
