cf400d46002f6ce5b55a738166d46e7af4edd761
Paul McMahon <colpanius@gmail.com>
2013-06-17 17:18:04 -0700

    Added work done on day 1 of gluestick template

---
+++ gluestick-template/index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Gluestick Marketing</title>
+	<link rel="stylesheet" href="<?php
+			echo get_template_directory_uri();
+			?>/style.css" />
+	<link href='http://fonts.googleapis.com/css?family=Signika+Negative:300,400' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+	<div class="row">
+		<div class="title columns">
+			<p>gluestick marketing</p>
+		</div>
+		<div class="social columns">
+			<img src="<?php
+			echo get_template_directory_uri();
+			?>
+			/images/socialbuttons.png" />
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="topbar">
+			<img src="<?php
+			echo get_template_directory_uri();
+			?>
+			/images/homebutton.png" />
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="main">
+			<div class="randomwhite">
+			</div>
+			<div class="textbig">
+			</div>
+			<div class="textbottom">
+			</div>
+		</div>
+	</div>
+
+	<div class="row special">
+
+			<div class="icons columns">
+				<h1>WHAT WE DO</h1>
+				<img src="<?php
+					echo get_template_directory_uri();
+					?>
+					/images/icon2.png" />
+			</div>
+
+			<div class="infotext columns">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed d</p>
+				<a href="" class="readmore"></a>
+			</div>
+	</div>
+
+
+	<div class="row special">
+
+			<div class="infotext columns">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed d</p>
+				<a href="" class="readmore"></a>
+			</div>
+
+			<div class="icons columns">
+				<h1>WHO WE ARE</h1>
+				<img src="<?php
+					echo get_template_directory_uri();
+					?>
+					/images/icon3.png" />
+			</div>
+
+	</div>
+
+	<div class="row special">
+
+			<div class="icons columns">
+				<h1>CONTACT US</h1>
+				<img src="<?php
+					echo get_template_directory_uri();
+					?>
+					/images/icon4.png" />
+			</div>
+
+			<div class="infotext columns">
+				<h2>gluestick marketing</h2>
+				<h3>
+					4542 ruffner st. suite 170 <br />
+					san diego ca, 92111
+				</h3>
+				<h4>
+					123.456.7890
+					nicole@gluestickmarketing.com
+				</h4>
+			</div>
+
+	</div>
+
+	<div class="row">
+		<div class="checkout columns">
+			<div class="checkusout">
+				<h5>don't forget to check out our blog &#187;</h5>
+			</div>
+
+			<img src="<?php
+				echo get_template_directory_uri();
+				?>
+				/images/icon5.png" />
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="footer">
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">About Us</a></li>
+				<li><a href="">Services</a></li>
+				<li><a href="">Blog</a></li>
+				<li><a href="">Team</a></li>
+				<li><a href="">Contact Us</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+
+</html>
