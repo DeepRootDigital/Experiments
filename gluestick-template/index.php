cc9326c74adbd5d70e0794a9332b122eade466d1
Paul McMahon <colpanius@gmail.com>
2013-06-18 20:13:15 -0700

    Finished gluestick template - 7 hours

--- gluestick-template/index.php
+++ gluestick-template/index.php
@@ -7,7 +7,8 @@
 	<link rel="stylesheet" href="<?php
 			echo get_template_directory_uri();
 			?>/style.css" />
-	<link href='http://fonts.googleapis.com/css?family=Signika+Negative:300,400' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Signika+Negative:300,400,600' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Dosis:500' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
@@ -26,10 +27,20 @@
 
 	<div class="row">
 		<div class="topbar">
-			<img src="<?php
-			echo get_template_directory_uri();
-			?>
-			/images/homebutton.png" />
+			<a href="" class="firstmenu">
+				<div class="menushadow">
+					<img src="<?php
+						echo get_template_directory_uri();
+						?>
+						/images/homebutton.png" />
+				</div>
+			</a>
+			
+			<a href="" class="menu">About Us</a>
+			<a href="" class="menu">Services</a>
+			<a href="" class="menu">Blog</a>
+			<a href="" class="menu">Team</a>
+			<a href="" class="lastmenu">Contact Us</a>
 		</div>
 	</div>
 
@@ -38,34 +49,56 @@
 			<div class="randomwhite">
 			</div>
 			<div class="textbig">
+				<p><span>Lorem</span> ipsum dolor sit amet, consectetur adipisicing elit, 
+					sed do eiusmod tempor.</p>
 			</div>
 			<div class="textbottom">
+				<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+					tempor incididunt ut labore et dolore magna aliqua”</p>
 			</div>
 		</div>
 	</div>
 
-	<div class="row special">
+
+	<div class="row">
+	<div class="contentbackground">
+		<div class="row">
 
 			<div class="icons columns">
 				<h1>WHAT WE DO</h1>
 				<img src="<?php
 					echo get_template_directory_uri();
 					?>
-					/images/icon2.png" />
+					/images/icon2.png" id="cogs" />
 			</div>
 
 			<div class="infotext columns">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed d</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+						enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+						nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+						pariatur. </p>
 				<a href="" class="readmore"></a>
 			</div>
-	</div>
+			<div class="dots">
+				<img src="<?php
+					echo get_template_directory_uri();
+					?>
+					/images/dots.png" />
+			</div>
+		</div>
 
-
-	<div class="row special">
+	<div class="row">
 
 			<div class="infotext columns">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed d</p>
-				<a href="" class="readmore"></a>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do 
+						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+						enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+						nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+						reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+						pariatur. </p>
+					<a href="" class="readmore"></a>
 			</div>
 
 			<div class="icons columns">
@@ -73,19 +106,24 @@
 				<img src="<?php
 					echo get_template_directory_uri();
 					?>
-					/images/icon3.png" />
+					/images/icon3.png" id="people" />
 			</div>
-
+			<div class="dots">
+				<img src="<?php
+					echo get_template_directory_uri();
+					?>
+					/images/dots.png" />
+			</div>
 	</div>
 
-	<div class="row special">
-
+	<div class="row">
+		<div class="row">
 			<div class="icons columns">
 				<h1>CONTACT US</h1>
 				<img src="<?php
 					echo get_template_directory_uri();
 					?>
-					/images/icon4.png" />
+					/images/icon4.png" id="letter" />
 			</div>
 
 			<div class="infotext columns">
@@ -95,17 +133,27 @@
 					san diego ca, 92111
 				</h3>
 				<h4>
-					123.456.7890
+					123.456.7890 <br />
 					nicole@gluestickmarketing.com
 				</h4>
 			</div>
+		</div>
 
-	</div>
+			<div class="dots">
+				<img src="<?php
+					echo get_template_directory_uri();
+					?>
+					/images/dots.png" />
+			</div>
+
+		</div>
+	
+
 
 	<div class="row">
 		<div class="checkout columns">
 			<div class="checkusout">
-				<h5>don't forget to check out our blog &#187;</h5>
+				<h5>don't forget to check out our blog  &nbsp; &#187;</h5>
 			</div>
 
 			<img src="<?php
@@ -114,17 +162,21 @@
 				/images/icon5.png" />
 		</div>
 	</div>
+</div>
+	</div>
 
 	<div class="row">
 		<div class="footer">
-			<ul>
-				<li><a href="">Home</a></li>
-				<li><a href="">About Us</a></li>
-				<li><a href="">Services</a></li>
-				<li><a href="">Blog</a></li>
-				<li><a href="">Team</a></li>
-				<li><a href="">Contact Us</a></li>
-			</ul>
+			<div class="footerborder">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">About Us</a></li>
+					<li><a href="">Services</a></li>
+					<li><a href="">Blog</a></li>
+					<li><a href="">Team</a></li>
+					<li><a href="">Contact Us</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 
