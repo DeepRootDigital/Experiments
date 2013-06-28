2d8e56097d1c5b36a5313a57db2ad099644441bf
Paul McMahon <colpanius@gmail.com>
2013-06-27 17:00:55 -0700

    BMS News - 4 hours

---
+++ bms-news/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>bms news</title>
+	<link tyep="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div id="wrapper">
+	<div class="row">
+		<div class="news-header">
+			<div class="news-header-header column">
+				<h1>BMS Client Buzz //</h1>
+			</div>
+			<div class="news-header-price column">
+				<p><span>May</span><br />
+				17, 2013<br />
+				<span>10</span> <br />
+				cents</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="main-headline">
+			<p><span>Body Beanz Website</span> is a <span>success</span> days after launch</p>
+		</div>
+	</div>
+	<div class="row news-area">
+		<div class="left-news-column column">
+			<div class="left-news-article">
+				<div class="left-news-column-heading">
+				</div>
+				<div class="left-news-column-content">
+				</div>
+			</div>
+			<div class="left-news-article">
+				<div class="left-news-column-heading">
+				</div>
+				<div class="left-news-column-content">
+				</div>
+			</div>
+			<div class="left-news-article">
+				<div class="left-news-column-heading">
+				</div>
+				<div class="left-news-column-content">
+				</div>
+			</div>
+		</div>
+		<div class="right-news-column column">
+			<div class="right-news-headliner">
+			</div>
+			<div class="right-news-quote">
+			</div>
+			<div class="right-news-article">
+			</div>
+			<div class="right-news-article">
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="back-to-top">
+		</div>
+	</div>
+	</div>
+</body>
+</html>
